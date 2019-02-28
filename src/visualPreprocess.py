4a9397586af72e158151b32336c1c454c0a402cf
Nick Kirkpatrick <nicholasmichaelkirkpatrick@gmail.com>
2019-02-27 21:05:14 -0600

    better error handling

--- src/visualPreprocess.py
+++ src/visualPreprocess.py
@@ -8,22 +8,23 @@ import i3ipc
 i3 = i3ipc.Connection()
 # pdb.set_trace()
 with mss.mss() as sct:
-    # Part of the screen to capture
-
     while "Screen capturing":
         last_time = time.time()
-        tree=i3.get_tree()
+        tree=i3.get_tree() #this line must be in the loop for us to approppriately react to widnow size changes
         print(sct.monitors)
-        # wind=tree.find_named("VLC")
         wind=tree.find_named("VLC")
+        #use absolute rather than relative cordinates
+        # windAbs = {"top": 90000, "left": wind[0].rect.x, "width": wind[0].rect.width, "height": wind[0].rect.height}
         windAbs = {"top": wind[0].rect.y, "left": wind[0].rect.x, "width": wind[0].rect.width, "height": wind[0].rect.height}
         img =None
         try:
             img = numpy.array(sct.grab(windAbs))
-        except:
-        # except ScreenShotError:
+        # except:
+        except mss.exception.ScreenShotError:
             details = sct.get_error_details()
+            print("error! Quiting")
             print(details)
+            quit()
         cv2.imshow('windAbs', cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
         print(windAbs)
         print("----------------------------")

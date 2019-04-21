b33bee6c73181a17e4235ae0373d1f1e253d6407
NK <fozbstudios@gmail.com>
2019-04-21 17:09:57 -0500

    progress on windows but may not be doable

--- src/backgroundKeystrokesToSuperflight.py
+++ src/backgroundKeystrokesToSuperflight.py
@@ -1,8 +1,72 @@
-import time
+# direct inputs
+# source to this solution and code:
+# http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
+# http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
 
-from pywinauto import application, keyboard 
-appl=application.Application()
-app = appl.connect(process=19364)
-app.top_window().type_keys("k")
-# time.sleep(15)
-# print("sending keys to superflight in 15 seconds! alt tab now")
+import ctypes
+import time
+import win32api
+import win32con
+import win32gui
+SendInput = ctypes.windll.user32.SendInput
+
+
+W = 0x11
+A = 0x1E
+S = 0x1F
+D = 0x20
+
+NP_2 = 0x50
+NP_4 = 0x4B
+NP_6 = 0x4D
+NP_8 = 0x48
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+win32api.PostMessage(265324, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+win32api.PostMessage(265324, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
